352d13c9ce5c15482d8278dcac03d9ebff1c81b3
Miguel de la Cruz <miguel@mcrx.me>
2020-12-13 11:55:55 +0100

    Add params decoding

--- server/http.go
+++ server/http.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
+
+type Params map[string]interface{}
+
+func DecodeParams(r *http.Request) *Params {
+	p := &Params{}
+	json.NewDecoder(r.Body).Decode(&p)
+	return p
+}
+
+func (p *Params) String(key string) string {
+	v, _ := (*p)[key].(string)
+	return v
+}
 
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
 	url := r.URL.String()
@@ -13,9 +27,14 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 	case (url == "/" || url == "") && r.Method == "GET":
 		fmt.Fprintf(w, "ROOT")
 	case url == "/create" && r.Method == "POST":
-		fmt.Fprintf(w, "CREATE")
+		p := DecodeParams(r)
+
+		w.WriteHeader(201)
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, "name=%s", p.String("name"))
+	case url == "/remove" && r.Method == "DELETE":
+		fmt.Fprintf(w, "DELETE")
 	default:
-		w.WriteHeader(404)
-		fmt.Fprintf(w, "Not Found")
+		http.NotFound(w, r)
 	}
 }
